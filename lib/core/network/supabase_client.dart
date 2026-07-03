78ccab8b23637c046e1d8eb93ebd806afb8c7ca7
Reverseflash45 <rffernanditoo@gmail.com>
2026-07-03 20:28:18 +0700

    Update project

--- lib/core/network/supabase_client.dart
+++ lib/core/network/supabase_client.dart
@@ -3,8 +3,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class SupabaseClientConfig {
   static Future<void> initialize() async {
     await Supabase.initialize(
-      url: 'ISI_DENGAN_URL_SUPABASE_LU',
-      anonKey: 'ISI_DENGAN_ANON_KEY_SUPABASE_LU',
+      url: 'https://otgdedgfwrzdsxvvsuep.supabase.co',
+      anonKey: 'sb_publishable_DFHjPAdZdp460dMNFu7RjA_ryQyg4qN',
     );
   }
 
