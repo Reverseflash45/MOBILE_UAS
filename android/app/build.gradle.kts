844c450d98f297f515d698a148ef77d22c4b6538
Reverseflash45 <rffernanditoo@gmail.com>
2026-07-05 19:16:37 +0700

    Update project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true // <-- Ditambahkan untuk mengatasi error
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -41,4 +42,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// <-- Blok dependencies ditambahkan di sini
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
